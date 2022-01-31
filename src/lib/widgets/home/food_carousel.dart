479e045997601bd209d9f7118ba87269f358941d
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-31 22:05:44 +0900

    overlayの中身を実装

--- src/lib/widgets/home/food_carousel.dart
+++ src/lib/widgets/home/food_carousel.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sample/utils/colors.dart';
+import 'package:sample/widgets/icon_and_text_widget.dart';
+import 'package:sample/widgets/large_text.dart';
+import 'package:sample/widgets/small_text.dart';
 
 class FoodCarousel extends StatefulWidget {
   const FoodCarousel({Key? key}) : super(key: key);
@@ -8,7 +12,6 @@ class FoodCarousel extends StatefulWidget {
 }
 
 class _FoodCarouselState extends State<FoodCarousel> {
-
   /// [PageView]のコントローラ
   final _pageController = PageController(viewportFraction: 0.85);
 
@@ -32,7 +35,7 @@ class _FoodCarouselState extends State<FoodCarousel> {
         Container(
           height: 220,
           width: double.maxFinite,
-          margin: const EdgeInsets.symmetric(horizontal: 5),
+          margin: const EdgeInsets.symmetric(horizontal: 10),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(30),
             child: Image.asset(
@@ -44,11 +47,71 @@ class _FoodCarouselState extends State<FoodCarousel> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: const EdgeInsets.only(left: 40, right: 40, bottom: 15,),
+            height: 120,
+            width: double.maxFinite,
+            margin: const EdgeInsets.only(
+              left: 30,
+              right: 30,
+              bottom: 30,
+            ),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: Colors.white,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 15,
+                top: 10,
+                right: 15,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const LargeText(text: 'Chinese Side'),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => const Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const SmallText(text: '4.5'),
+                      const SizedBox(width: 10),
+                      const SmallText(text: '1287'),
+                      const SizedBox(width: 10),
+                      const SmallText(text: 'comments'),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      IconAndTextWidget(
+                        icon: Icons.circle_sharp,
+                        text: 'Normal',
+                        iconColor: AppColors.iconColor1,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.location_on,
+                        text: '1.7km',
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.access_time_rounded,
+                        text: '32min',
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
