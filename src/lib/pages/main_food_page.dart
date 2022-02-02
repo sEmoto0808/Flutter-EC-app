4fb15a05d465741b418cf67b9b733963f3e609b1
Sho Emoto <dd2d00seg1@gmail.com>
2022-02-02 20:55:24 +0900

    ヘッダーはスクロールしないように修正

--- src/lib/pages/main_food_page.dart
+++ src/lib/pages/main_food_page.dart
@@ -15,76 +15,80 @@ class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // ヘッダー
-            Container(
-              margin: const EdgeInsets.only(top: 45, bottom: 15),
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      const LargeText(
-                        text: 'Japan',
-                        color: AppColors.mainColor,
-                      ),
-                      Row(
-                        children: const [
-                          SmallText(
-                            text: 'Tokyo',
-                            color: Colors.black54,
-                          ),
-                          Icon(Icons.arrow_drop_down_rounded),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: 45,
-                    height: 45,
-                    decoration: BoxDecoration(
+      body: Column(
+        children: [
+          // ヘッダー
+          Container(
+            margin: const EdgeInsets.only(top: 45, bottom: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    const LargeText(
+                      text: 'Japan',
                       color: AppColors.mainColor,
-                      borderRadius: BorderRadius.circular(15),
                     ),
-                    child: const Icon(
-                      Icons.search,
-                      color: Colors.white,
+                    Row(
+                      children: const [
+                        SmallText(
+                          text: 'Tokyo',
+                          color: Colors.black54,
+                        ),
+                        Icon(Icons.arrow_drop_down_rounded),
+                      ],
                     ),
+                  ],
+                ),
+                Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: AppColors.mainColor,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const FoodCarousel(),
+                  // list of food
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 120,
+                              height: 120,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.asset('assets/images/food0.png'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ],
               ),
             ),
-            const FoodCarousel(),
-            // list of food
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const BouncingScrollPhysics(),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 120,
-                  height: 120,
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        height: 120,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset('assets/images/food0.png'),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
