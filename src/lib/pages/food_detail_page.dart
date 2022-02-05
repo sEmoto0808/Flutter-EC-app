fd2060205fb1a454fbf0ae34ef61df6d231b1c5a
Sho Emoto <dd2d00seg1@gmail.com>
2022-02-05 22:25:40 +0900

    FoodDetailPageを作成

---
+++ src/lib/pages/food_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodDetailPage extends StatelessWidget {
+  const FoodDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
